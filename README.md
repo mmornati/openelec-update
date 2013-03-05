2ab7188edd81bb5157d70b625d7fc7066268d431
Marco Mornati <ilmorna@gmail.com>
2013-03-05 22:49:29 +0100

    Added script and README

--- README.md
+++ README.md
@@ -2,3 +2,45 @@ openelec-update
 ===============
 
 OpenElec for RaspberryPI Update Script
+
+Installation Instruction
+------------------------
+To use this script you should copy it on your raspberry OpenElec 
+
+----------------------------------------------------------------
+scp update.sh root@openelec:
+----------------------------------------------------------------
+
+The default root user for openelec distribution is 'openelec'.
+
+Then you should to connect via ssh to your OpenElec and execute the script.
+It will automatically check if a new version is available, it shown to you the
+new version name and ask for a confirmation.
+
+----------------------------------------------------------------
+root ~ # ./update.sh 
+Update required, will download latest version.
+Current Version: r13395
+Latest Version: r13450 (OpenELEC-RPi.arm-devel-20130305190626-r13450.tar.bz2)
+Continue (y/n)?y
+Connecting to openelec.thestateofme.com (46.149.19.9:80)
+OpenELEC-RPi.arm-dev 100%
+Download complete
+
+Uncompressing tarball, files extracted:
+OpenELEC-RPi.arm-devel-20130305190626-r13450/
+....
+....
+
+OpenELEC files succesfully moved to update directory
+Temporary files deleted
+System will restart shortly
+Enjoy!
+----------------------------------------------------------------
+
+
+If you decide to install the new version, it will download the OpenElec tar.gz
+file, decompress it in the update folder of the openelec distribution and
+restart the RaspberryPI.
+
+Installation and Update Done!
---
+++ update.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# change working directory 
+cd /storage
+
+# location of the nightlies
+url="http://openelec.thestateofme.com/"
+
+# get base, revision and filename of last build
+last_base=`curl -s $url | grep .tar.bz2 | sed 's/.*\(OpenELEC-RPi.*\).tar.bz2.*/\1/' | sort | tail -1`
+last_revision=`echo $last_base | sed 's/.*\(r[0-9]*\)/\1/'`
+last_filename=$last_base.tar.bz2
+
+# folder name is set equal to base
+foldername=$last_base
+
+# get currently installed revision
+this_revision=`cat /etc/version | sed 's/.*\(r[0-9]*\)/\1/'`
+# check if currently installed revision is up-to-date
+if [ $this_revision == $last_revision ]
+then
+    echo "System is up-to-date, no update required."
+    exit
+else
+    echo "Update required, will download latest version."
+fi
+
+echo "Current Version: $this_revision"
+echo "Latest Version: $last_revision ($last_filename)"
+
+read -p "Continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) 
+     #do nothing and go ahead
+     ;;
+  n|N ) 
+      exit
+      ;;
+  * ) 
+     exit
+     ;;
+esac
+
+# clean up previously interrupted update
+if [ -a $last_filename ]; then
+    echo "Clean up previously interrupted update files."
+    rm $last_filename
+fi
+if [ -a $foldername ]; then
+    echo "Clean up previously interrupted update files."
+    rm -rf $foldername
+fi
+
+# download corresponding file to working directory
+urltolast=$url/$last_filename
+wget $urltolast
+echo -e  "Download complete\n"
+
+# uncompressing the tarball
+echo "Uncompressing tarball, files extracted:"
+tar -xvjf $last_filename
+
+# check if image folder exists, otherwise exit
+if [ ! -d $foldername ]; then
+   echo "Cannot find extracted folder."
+   exit
+fi
+
+# check if .update folder exists, otherwise create it
+if [ ! -d /storage/.update ]; then
+    mkdir /storage/.update
+fi
+
+# move OpenELEC files (including .md5 files) to update folder
+mv $foldername/target/* /storage/.update/
+echo -e "\nOpenELEC files succesfully moved to update directory"
+
+# clean up
+rm -r $foldername
+rm $last_filename
+echo "Temporary files deleted"
+
+# sync and reboot system to apply updates
+echo "System will restart shortly"
+echo "Enjoy!"
+sleep 5s
+sync
+reboot
